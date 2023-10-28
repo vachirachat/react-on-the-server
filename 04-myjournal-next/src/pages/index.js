255cf287d3c34100757a123364ffc1a53e64bfc8
P-Net <Net-Vachirachat@Vachirachats-MacBook-Pro-2.local>
2023-10-28 14:49:02 +0700

    fix bug 1

--- 04-myjournal-next/src/pages/index.js
+++ 04-myjournal-next/src/pages/index.js
@@ -1,14 +1,41 @@
-import { Typography } from "@mui/joy";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
 import Container from "@mui/joy/Container";
 import Head from "next/head";
+import Link from "next/link";
+import { ArticleList } from "../components/ArticleList";
+import { db } from "../db";
 
-export default function ArticleListPage() {
+/**
+ *
+ * @param {import("next").InferGetServerSidePropsType<typeof getServerSideProps>} props
+ * @returns
+ */
+export default function ArticleListPage({ data }) {
   return (
     <Container sx={{ py: 2 }}>
       <Head>
         <title>My Journal</title>
       </Head>
-      <Typography variant="h1">Article List Page</Typography>
+      <Box textAlign="right" mb={2}>
+        <Button component={Link} href="/add">
+          Add article
+        </Button>
+      </Box>
+      <ArticleList data={data} />
     </Container>
   );
 }
+
+export const getServerSideProps = async () => {
+  const data = await db.listArticles();
+
+  return {
+    props: {
+      data: data.map((item) => ({
+        ...item,
+        createdAt: item.createdAt.toISOString(),
+      })),
+    },
+  };
+};
