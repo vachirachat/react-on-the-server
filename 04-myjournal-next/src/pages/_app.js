765b05208d647b2d922f0b6227bfec035bff5686
P-Net <Net-Vachirachat@Vachirachats-MacBook-Pro-2.local>
2023-10-28 14:51:46 +0700

    delete unused react-query

--- 04-myjournal-next/src/pages/_app.js
+++ 04-myjournal-next/src/pages/_app.js
@@ -1,14 +1,8 @@
-import { useState } from "react";
 import { Navigation } from "@/components/Navigation";
 import { CacheProvider } from "@emotion/react";
 import { CssVarsProvider } from "@mui/joy/styles";
 import CssBaseline from "@mui/joy/CssBaseline";
 import createEmotionCache from "@/createEmotionCache";
-import {
-  QueryClient,
-  QueryClientProvider,
-  HydrationBoundary,
-} from "@tanstack/react-query";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/700.css";
 import "@fontsource/inter/900.css";
@@ -20,18 +14,13 @@ export default function App({
   pageProps,
   emotionCache = clientSideEmotionCache,
 }) {
-  const [queryClient] = useState(() => new QueryClient());
   return (
-    <QueryClientProvider client={queryClient}>
-      <HydrationBoundary state={pageProps.dehydratedState}>
-        <CacheProvider value={emotionCache}>
-          <CssVarsProvider>
-            <CssBaseline />
-            <Navigation />
-            <Component {...pageProps} />
-          </CssVarsProvider>
-        </CacheProvider>
-      </HydrationBoundary>
-    </QueryClientProvider>
+    <CacheProvider value={emotionCache}>
+      <CssVarsProvider>
+        <CssBaseline />
+        <Navigation />
+        <Component {...pageProps} />
+      </CssVarsProvider>
+    </CacheProvider>
   );
 }
